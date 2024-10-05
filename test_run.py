5edb1a2f257757bd4043e6c0a7efce8635185e0b
Minho choi <mindori17@naver.com>
2024-10-05 22:40:19 +0900

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -1,7 +1,14 @@
 import os
+import numpy as np
 
 print("start cord")
 path = "/root"
 file_list = os.listdir(path)
 
-print ("file_list: {}".format(file_list))
+# print ("file_list: {}".format(file_list))
+path_data = "C:/Users/minho/Desktop/work/kiom/PPG_resp/algorithm/matlab/data1_Capno.mat"
+mat_file = loadmat(path_data)
+data = mat_file['data']
+
+a =3
+np.savez('/result/results', a=a)
